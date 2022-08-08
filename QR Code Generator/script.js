68608aaf2d15635c485d00c9bc3fe3ba9033d8e3
Sanidhya Kashyap <sanidhyak7@gmail.com>
2022-08-08 21:12:11 +0530

    first commit

---
+++ QR Code Generator/script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper"),
+qrinput = wrapper.querySelector(".form input"),
+documentBtn = wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img")
+
+
+documentBtn.addEventListener("click", ()=>{
+    let qrValue = qrinput.value;
+    if(!qrValue) return;
+    documentBtn.innerText = "Generating QR Code..."
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+    qrImg.addEventListener("load", () =>{
+        wrapper.classList.add("active");
+        documentBtn.innerText = "Generate QR Code"
+
+    });
+});
+
+qrinput.addEventListener("keyup", () =>{
+    if(!qrinput.value) {
+        wrapper.classList.remove("active");
+    }
+});
